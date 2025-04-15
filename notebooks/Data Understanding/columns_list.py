bc5db6877c9812afdbef8f94d5cfb26941bd294c
André Costa <costa.andre.2012@gmail.com>
2025-04-15 13:59:43 +0100

    Filtered special cases in student data + added commentaries in notebook

--- notebooks/Data Understanding/columns_list.py
+++ notebooks/Data Understanding/columns_list.py
@@ -10,3 +10,29 @@ math_subscales = [
 reading_columns = [f"PV{i}READ" for i in range(1, 11)]
 
 science_columns = [f"PV{i}SCIE" for i in range(1, 11)]
+
+
+def get_math_results(dataframe, math_columns):
+    math_results = dataframe.copy()
+    math_results["Avg Math Result"] = math_results[math_columns].mean(axis=1)
+    math_results = math_results.drop(columns=math_columns)
+
+    return math_results
+
+
+def get_science_results(dataframe, science_columns):
+    scie_results = dataframe.copy()
+    scie_results["Avg Science Result"] = scie_results[science_columns].mean(axis=1)
+    scie_results = scie_results.drop(columns=science_columns)
+
+    return scie_results
+
+
+def get_reading_results(dataframe, reading_columns):
+    reading_results = dataframe.copy()
+    reading_results["Avg Reading Result"] = reading_results[reading_columns].mean(
+        axis=1
+    )
+    reading_results = reading_results.drop(columns=reading_columns)
+
+    return reading_results
